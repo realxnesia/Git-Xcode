2b77d94177c7811ab9d1dafa31ea1dc348da4b68
realxnesia <dhikaaditya1@gmail.com>
2022-07-21 14:04:59 +0700

    change main to main v2

--- Git-Xcode/ViewController.swift
+++ Git-Xcode/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let nama = "Kevin"
-        let branch = "main"
+        let branch = "main-v2"
     }
 
 
